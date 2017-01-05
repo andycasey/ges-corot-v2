6ebc12adf289bd470438aab7aa8218999fd72408
Andy Casey <andycasey@gmail.com>
2017-01-05 18:28:40 +0000

    Moving furniture

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
+Gaia-ESO/CoRoT homogenisation
+=============================
+
+This repository contains software to perform the statistical homogenisation for the GES/CoRoT project.
+
+Authors
+-------
+
+  - Andrew R. Casey (andrew.casey@monash.edu)
+
+
+Usage
+-----
+ 
 If you know what you're doing:
 
 ``sh run_homogenisation.sh``
--- run_homogenisation.sh
+++ run_homogenisation.sh
@@ -1,9 +1,9 @@
 python scripts/setup_db.py
-python plot_quality_control.py
+python scripts/plot_quality_control.py
 python scripts/propagate_flags.py
 python scripts/homogenise_giraffe.py
 python scripts/plot_giraffe_homogenisation_diagnostics.py
 python scripts/homogenise_uves.py
 python scripts/plot_uves_homogenisation_diagnostics.py
-python scripts/plot_homogenisation_results.py
+python scripts/plot_homogenisation_quality_control.py
 python scripts/shipit.py
