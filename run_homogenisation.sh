793a6a8e06464caad14c22ff7996ff623ec1af33
Andy Casey <andycasey@gmail.com>
2017-01-06 11:18:04 +0000

    Typo

--- run_homogenisation.sh
+++ run_homogenisation.sh
@@ -5,6 +5,6 @@ python scripts/homogenise_giraffe.py
 python scripts/plot_giraffe_homogenisation_diagnostics.py
 python scripts/homogenise_uves.py
 python scripts/plot_uves_homogenisation_diagnostics.py
-python scripts/plot_homogenisation_quality_control.py
+python scripts/plot_homogenised_quality_control.py
 tar -cvzf outputs/ges-corot-homogenisation-figures.tar.gz figures/*
 python scripts/shipit.py
